f78defa8dc6f005cf50140f9b76609c8f2db45a0
Luciele Rodrigues Soares <68296340+luciele22@users.noreply.github.com>
2020-07-15 12:59:47 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+//../ saio da pasta
+//./eu indico que estou na raiz da pasta origem.
+var usuario = {
+    nome:'Fulano',
+    senha:"12334565",
+    podeAcessarRecursoEscondido: false
+};
+
+
+var meuModulo = (function(){
+
+}
